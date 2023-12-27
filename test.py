63831bcbd4dfb94b737b1a6902f42545d9f3e358
itamarKovach <148196311+itamarKovach@users.noreply.github.com>
2023-12-27 17:56:26 +0200

    Update test.py

--- test.py
+++ test.py
@@ -3,13 +3,13 @@ import time
 
 def start_server():
     print("Starting server...")
-    subprocess.run(["python", "Server.py"])  # Replace "server_script.py" with the actual filename of your server script
+    subprocess.run(["python", "Server.py"])
 
 def start_client():
-    # You might want to add a delay here to ensure the server has started before the client attempts to connect
+    # add a delay to ensure the server has started before the client attempts to connect
     time.sleep(2)
     print("Starting client...")
-    subprocess.run(["python", "Client.py"])  # Replace "client_script.py" with the actual filename of your client script
+    subprocess.run(["python", "Client.py"])
 
 if __name__ == "__main__":
     server_process = subprocess.Popen(["python", "-c", "from test import start_server; start_server()"])
